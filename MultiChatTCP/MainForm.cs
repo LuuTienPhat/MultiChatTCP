ad7a98fad1cbd83bbc4c76d803e08185313d0966
Lưu Tiến Phát <luutienphat@gmail.com>
2021-12-24 22:37:48 +0700

    Add project files.

---
+++ MultiChatTCP/MainForm.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace MultiChatTCP
+{
+    public partial class MainForm : DevExpress.XtraEditors.XtraForm
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void labelControl1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void richEditControl1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
