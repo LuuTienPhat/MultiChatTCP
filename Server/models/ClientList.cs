ab5d7e96c6361eb9f06fd807c9776a565901d706
Lưu Tiến Phát <luutienphat@gmail.com>
2021-12-26 23:53:09 +0700

    Create intance to the client list, when there is new client connects to server, add that TcpClient object to the list.

---
+++ Server/models/ClientList.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.Net.Sockets;
+
+namespace Server
+{
+    public sealed class ClientList
+    {
+        private List<TcpClient> clients = new List<TcpClient>();
+        public List<TcpClient> Clients
+        {
+            get { return clients; }
+            //set { clients = value; }
+        }
+
+        private static readonly object padlock = new object();
+        private static ClientList instance = null;
+        private ClientList() { }
+        public static ClientList Instance
+        {
+            get
+            {
+                lock (padlock)
+                {
+                    if (instance == null)
+                    {
+                        instance = new ClientList();
+                    }
+                    return instance;
+                }
+            }
+        }
+    }
+}
