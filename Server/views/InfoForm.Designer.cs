3a94a486a670b3d5371da7842fb436fd5987ae09
Lưu Tiến Phát <luutienphat@gmail.com>
2021-12-26 23:54:03 +0700

    Add info form to display all sockets connect to server

---
+++ Server/views/InfoForm.Designer.cs
@@ -0,0 +1,59 @@
+﻿namespace Server
+{
+    partial class InfoForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBoxControl1 = new DevExpress.XtraEditors.ListBoxControl();
+            ((System.ComponentModel.ISupportInitialize)(this.listBoxControl1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // listBoxControl1
+            // 
+            this.listBoxControl1.Location = new System.Drawing.Point(88, 52);
+            this.listBoxControl1.Name = "listBoxControl1";
+            this.listBoxControl1.Size = new System.Drawing.Size(346, 145);
+            this.listBoxControl1.TabIndex = 0;
+            // 
+            // InfoForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(512, 267);
+            this.Controls.Add(this.listBoxControl1);
+            this.Name = "InfoForm";
+            this.Text = "InfoForm";
+            ((System.ComponentModel.ISupportInitialize)(this.listBoxControl1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private DevExpress.XtraEditors.ListBoxControl listBoxControl1;
+    }
+}
---
+++ Server/views/InfoForm.cs
@@ -0,0 +1,14 @@
+﻿namespace Server
+{
+    public partial class InfoForm : DevExpress.XtraEditors.XtraForm
+    {
+        public InfoForm()
+        {
+            InitializeComponent();
+            foreach (var client in ClientList.Instance.Clients)
+            {
+                listBoxControl1.Items.Add(client.Client.RemoteEndPoint);
+            }
+        }
+    }
+}
